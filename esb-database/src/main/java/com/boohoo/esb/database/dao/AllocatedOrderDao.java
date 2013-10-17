1852fbfc1608e089caeaf8fbdc04e4d126bb546d
Ramesh Rajendran <rameschinnu61@gmail.com>
2013-10-17 22:46:17 +0100

    working database with hibernate

--- esb-database/src/main/java/com/boohoo/esb/database/dao/AllocatedOrderDao.java
+++
@@ -1,9 +0,0 @@
-package com.boohoo.esb.database.dao;
-
-import com.boohoo.esb.database.dao.model.AllocatedOrder;
-
-public interface AllocatedOrderDao extends BaseDao<AllocatedOrder, Integer> {
-	public Integer createAllocatedOrder(Integer orderNumber, Integer orderStatus, Integer insertId);
-	public Integer updateAllocatedOrderStatus(Integer orderStatus, Integer id);
-	public Integer getAllocatedOrderId(String orderNumber);
-}
--- esb-database/src/main/java/com/boohoo/esb/database/dao/SageMasterStockDao.java
+++ esb-database/src/main/java/com/boohoo/esb/database/dao/SageMasterStockDao.java
@@ -5,6 +5,6 @@ import java.util.List;
 import com.boohoo.esb.database.dao.model.SageMasterStock;
 
 public interface SageMasterStockDao extends BaseDao<SageMasterStock, Integer> {
-	public Integer updateSageStock(SageMasterStock masterStock);
+	public SageMasterStock updateSageStock(SageMasterStock masterStock);
 	public List<SageMasterStock> findAll();
 }
